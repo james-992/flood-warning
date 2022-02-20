e2578f869bf0845452f8a3eb8862d5acbe91f849
James <james.gibson5598@gmail.com>
2022-02-20 14:35:38 +0000

    Improving / fixing 2E

--- Task2E.py
+++ Task2E.py
@@ -1,66 +1,36 @@
-"""
-displays a plot of the water level data against time for a station, 
-and includes on the plot lines for the typical low and high levels.
-"""
 
 from floodsystem.datafetcher import *
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
 from datetime import datetime, timedelta
-import matplotlib.dates
+import matplotlib
 import matplotlib.pyplot as plt
 import datetime
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.stationdata import build_station_list
+from floodsystem.plot import plot_water_levels
 
-
-#this is code to build the dates levles data for the function to use
 
 #input the number of days of data you want to show
-dt = 2
-
+dt = 10
 # Station name to find
-station_name = "Cam"
-
+station_name = "Abingdon Lock"
 
 # Build list of stations
 stations = build_station_list()
-
-# Find station
 station_of_interest = None
-dates, levels = (fetch_measure_levels(station_of_interest.measure_id,
-                                     dt=datetime.timedelta(days=dt)))
-
-
 for station in stations:
     if station.name == station_name:
         station_of_interest = station
         break
 
-    if not station_of_interest:
-        raise ValueError("Station {} could not be found".format(station_name))
+if not station_of_interest:
+    raise ValueError("Station {} could not be found".format(station_name))
 
+# find the dates and the levels associated 
 
-#everything below here is the actual function
-
-
-def plot_water_levels(station, dates, levels):
-
-    # sets up the plot
-    
-    t = matplotlib.dates.date2num(dates)
-    plt.plot_date(t, levels)
-
-    # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('date')
-    plt.ylabel('water level (m)')
-    plt.xticks(rotation=45);
-    plt.title(station.name)
-
-    # Display plot
-    plt.tight_layout()  # This makes sure plot does not cut off date labels
-
-    plt.show()
-    
-    
+dates, levels = (fetch_measure_levels(station_of_interest.measure_id,
+                                     dt=datetime.timedelta(days=dt)))
+ 
 
 plot_water_levels(station_of_interest, dates, levels)
 
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,4 +1,50 @@
 """"
 for task 2E
 This sub module contains code for plotting data    
+displays a plot of the water level data against time for a station, 
+and includes on the plot lines for the typical low and high levels.
 """
+
+from floodsystem.datafetcher import *
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
+from datetime import datetime, timedelta
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+from floodsystem.datafetcher import fetch_measure_levels
+import matplotlib
+
+def plot_water_levels(station, dates, levels):
+
+    # sets up the dates
+
+    t = matplotlib.dates.date2num(dates)
+
+    #checks to see if the requested station has consistant upper and lower bounds which can be plotted
+    stations = build_station_list()
+    inconsistent_stations, inconsistent_data = inconsistent_typical_range_stations(stations)
+    if station in inconsistent_stations:
+        print ("cannot show typical ranges as the data is inconsistant")
+        pass
+    else:
+        lower, upper = station.typical_range
+        plt.axhline(y=upper, color='r', linestyle='-')
+        plt.axhline(y = lower, color = 'g', linestyle = '-')
+
+    
+    #makes the plot
+    plt.plot_date(t, levels)
+
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(station.name)
+
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
