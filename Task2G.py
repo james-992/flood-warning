ba910361adb30816a4e27158799821a6ee3d0743
jrb250 <jonah.r.barretto@gmail.com>
2022-03-02 20:46:57 +0000

    small additions

--- Task2G.py
+++ Task2G.py
@@ -1,9 +1,25 @@
 
 
-def run():
-    """Assessing flood risk based off the implementations done in both milestones. Rated from 'severe', 'high', 'moderate', and 'low'"""
+from floodsystem.stationdata import build_station_list, update_water_levels
 
+
+def run():
+    """Assessing flood risk based off the implementations done in both milestones. Rated from 'severe', 'high', 'moderate', and 'low.
+        Should list the TOWNS where flood risk is greatest'"""
     
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    #   WAYS OF MEASURING RISK
+    #   - stations_with_highest_rel_level()
+    #   - stations_over_threshold()
+
+    #   Weighted sum?
+    #   Find the change in river level over the previous couple days - is it increasing?
+    #   
+
+
+
 
 if __name__ == "__main__":
     print("Task 2G running...")
