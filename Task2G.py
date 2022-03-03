7c63e943db362bede5a95730ae7c6dcebb376aca
jrb250 <jonah.r.barretto@gmail.com>
2022-03-03 09:46:54 +0000

    additions

--- Task2G.py
+++ Task2G.py
@@ -3,7 +3,7 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import *
 from floodsystem.analysis import *
-from floodsystem.flood import stations_highest_rel_level
+from floodsystem.flood import stations_highest_rel_level, stations_over_threshold
 from floodsystem.plot import *
 from floodsystem.datafetcher import *
 import numpy
@@ -29,9 +29,9 @@ def find_rate_of_change(station):
     poly, d0 = polyfit(dates, levels, p)
     derivative2 = numpy.polyder(poly, m=1)   
     derivative2now = derivative2(0)
-    return(derivative1now/typicalrange, derivative2now/typicalrange)
+    return derivative1now/typicalrange, derivative2now/typicalrange
 
-N = 5
+"""N = 5
 stations = build_station_list()
 update_water_levels(stations)
 
@@ -39,17 +39,17 @@ for station in stations_highest_rel_level(stations, (N+1)):
     station_name = station[0]
     #specific_station = None
     if station[0] == "Letcombe Bassett":
-        print("Letcombe Bassett is not shown, it's data is bad")
+        print("Letcombe Bassett is not shown, it's data is codswallop")
         pass 
     else: 
         for station_obj in stations:
             if station_obj.name == station_name:
                 specific_station = station_obj
                 derivative1, derivative2 = find_rate_of_change(specific_station)
-                print(derivative1, derivative2)
+                print(station_name, derivative1, derivative2)
 
             else:
-                pass
+                pass"""
             
             
                 
@@ -74,6 +74,33 @@ def run():
     #   Weighted sum?
     #   Find the change in river level over the previous couple days - is it increasing? USE TASK 2F POLYNOMIAL - CALC DY/DX
     #   
+
+
+    tol = 1
+
+    #   find stations above arbitrary tolerance
+    stations_above_threshold_init = stations_over_threshold(stations, tol)
+
+    #   create empty list for the station obj to be appended to
+    stations_above_threshold = []
+
+    for station in stations_above_threshold_init:
+        
+        for station_obj in stations:
+            #   finding the station objects
+
+            if station[0] == station_obj.name:
+                stations_above_threshold.append(station_obj)
+
+    classification_list = []
+
+    for station in stations_above_threshold:
+        h, grad1, grad2 = find_rate_of_change()
+
+        risk_factor = 
+
+
+
 
 
 
